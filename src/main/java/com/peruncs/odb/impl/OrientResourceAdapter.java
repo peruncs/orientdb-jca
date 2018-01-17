cf001b92bf215b7d4f188610c5b413eb738c30db
hstoyano <hr.stoyanov@gmail.com>
2018-01-16 19:14:58 -0800

    Added resource adaptor

--- src/main/java/com/peruncs/odb/impl/OrientResourceAdapter.java
+++ src/main/java/com/peruncs/odb/impl/OrientResourceAdapter.java
@@ -1,27 +1,22 @@
-
 package com.peruncs.odb.impl;
 
-import javax.resource.ResourceException;
-import javax.resource.spi.ActivationSpec;
-import javax.resource.spi.BootstrapContext;
-import javax.resource.spi.Connector;
-import javax.resource.spi.ResourceAdapter;
-import javax.resource.spi.ResourceAdapterInternalException;
-import javax.resource.spi.TransactionSupport;
-import javax.resource.spi.endpoint.MessageEndpointFactory;
-import javax.transaction.xa.XAResource;
-
+import com.orientechnologies.orient.core.Orient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.orientechnologies.orient.core.Orient;
+import javax.resource.ResourceException;
+import javax.resource.spi.*;
+import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.transaction.xa.XAResource;
+
+import static javax.resource.spi.TransactionSupport.TransactionSupportLevel.LocalTransaction;
 
 
 @Connector(
-    transactionSupport = TransactionSupport.TransactionSupportLevel.LocalTransaction, 
-    version = "1.0",
-    vendorName = "PerunCS",
-    eisType = "OrientDB")
+        transactionSupport = LocalTransaction,
+        version = "1.0",
+        vendorName = "PerunCS",
+        eisType = "OrientDB")
 public class OrientResourceAdapter implements ResourceAdapter {
 
     static final XAResource[] EMPTY_XA_RESOURCES = new XAResource[0];
@@ -46,10 +41,12 @@ public class OrientResourceAdapter implements ResourceAdapter {
     }
 
     @Override
-    public void endpointActivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {}
+    public void endpointActivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {
+    }
 
     @Override
-    public void endpointDeactivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {}
+    public void endpointDeactivation(MessageEndpointFactory endpointFactory, ActivationSpec spec) {
+    }
 
     @Override
     public XAResource[] getXAResources(ActivationSpec[] specs) throws ResourceException {
